5b44028c1dc9e7fec4f07c1fba05e029cfa9a36d
Dmitry Lvov <>
2020-12-13 20:22:02 +0300

    lazy variant = drop table

--- app/src/main/java/com/tempvic/weather/MainApplication.java
+++ app/src/main/java/com/tempvic/weather/MainApplication.java
@@ -24,6 +24,7 @@ public class MainApplication extends Application {
                         "cityInfoDatabase"
                 )
                 .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
                 .build();
     }
 }
